3e23473db41377fe2e727036143278846b74ce58
Alon Ecker <a12345e@gmail.com>
2020-10-05 19:25:03 +0300

    Better not hold test check in file

---
+++ src/test/java/leg/agent/HistoryTest.java
@@ -0,0 +1,69 @@
+package leg.agent;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import leg.agent.util.CurrentResource;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public class HistoryTest extends SetupTest {
+    String inputSource;
+
+    private JsonElement computeExpectedJsonElement() throws IOException {
+        return JsonParser.parseString(Files.readString(Paths.get(CurrentResource.getTestExpectedJsonFile(1))));
+    }
+
+
+    @Test
+    public void historyFirstEvent() throws IOException {
+        Gson gson = new Gson();
+        VisitEvent ev = new VisitEvent(null, 10, new byte[]{1, 2, 3, (byte) 255});
+        PropertyChainBox pcb = new PropertyChainBox(null, new Properties());
+        pcb.set(PropertyChainBox.Property.HistoryPrefixLogLimit, 4);
+        pcb.set(PropertyChainBox.Property.HistorySuffixLogLimit, 2);
+        History history = new History(pcb);
+        history.visit(10, new byte[]{1, 2, 3, (byte) 255});
+
+        JsonElement jsonElement = gson.toJsonTree(history.getFirstEvent());
+        Assert.assertEquals(0, jsonElement.getAsJsonObject().get("time").getAsInt());
+        Assert.assertEquals(10, jsonElement.getAsJsonObject().get("step").getAsInt());
+        Assert.assertEquals(0, jsonElement.getAsJsonObject().get("visit").getAsInt());
+        Assert.assertEquals(1, jsonElement.getAsJsonObject().get("data").getAsJsonArray().get(0).getAsInt());
+        Assert.assertEquals(2, jsonElement.getAsJsonObject().get("data").getAsJsonArray().get(1).getAsInt());
+        Assert.assertEquals(3, jsonElement.getAsJsonObject().get("data").getAsJsonArray().get(2).getAsInt());
+        Assert.assertEquals(-1, jsonElement.getAsJsonObject().get("data").getAsJsonArray().get(3).getAsInt());
+    }
+
+    @Test
+    public void historyLastEvent() throws IOException {
+        Gson gson = new Gson();
+        VisitEvent ev = new VisitEvent(null, 10, new byte[]{1, 2, 3, (byte) 255});
+        PropertyChainBox pcb = new PropertyChainBox(null, new Properties());
+        pcb.set(PropertyChainBox.Property.HistoryPrefixLogLimit, 4);
+        pcb.set(PropertyChainBox.Property.HistorySuffixLogLimit, 2);
+        History history = new History(pcb);
+        history.visit(10, new byte[]{1, 2, 3, (byte) 255});
+        history.visit(11, new byte[]{1, 2, 4, (byte) 255});
+
+        JsonElement jsonElement = gson.toJsonTree(history.getLastEvent());
+        Assert.assertEquals(0, jsonElement.getAsJsonObject().get("time").getAsInt());
+        Assert.assertEquals(11, jsonElement.getAsJsonObject().get("step").getAsInt());
+        Assert.assertEquals(1, jsonElement.getAsJsonObject().get("visit").getAsInt());
+        Assert.assertEquals(1, jsonElement.getAsJsonObject().get("data").getAsJsonArray().get(0).getAsInt());
+        Assert.assertEquals(2, jsonElement.getAsJsonObject().get("data").getAsJsonArray().get(1).getAsInt());
+        Assert.assertEquals(4, jsonElement.getAsJsonObject().get("data").getAsJsonArray().get(2).getAsInt());
+        Assert.assertEquals(-1, jsonElement.getAsJsonObject().get("data").getAsJsonArray().get(3).getAsInt());
+
+    }
+
+    @Test
+    public void historyFull() throws IOException {
+
+    }
+}
